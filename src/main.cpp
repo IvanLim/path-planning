413058b728300cb74eb108d5aca44b73ccd7a49e
Ivan Lim <ivanlimsiukee@hotmail.com>
2017-09-22 20:44:29 +0800

    Implemented lane following and lane changing

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,12 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include "spline.h"
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+
+#define SPEED_LIMIT 49.5
 
 using namespace std;
 
@@ -85,7 +88,7 @@ int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x,
 // Transform from Cartesian x,y coordinates to Frenet s,d coordinates
 vector<double> getFrenet(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
 {
-	int next_wp = NextWaypoint(x,y, theta, maps_x,maps_y);
+	int next_wp = NextWaypoint(x, y, theta, maps_x, maps_y);
 
 	int prev_wp;
 	prev_wp = next_wp-1;
@@ -176,6 +179,16 @@ int main() {
 
   ifstream in_map_(map_file_.c_str(), ifstream::in);
 
+  // Keep track of vehicle lane number (starting in leftmost lane / lane 1)
+  int lane = 1;
+
+  // Reference velocity to keep within speed limit
+  double ref_vel = 0;
+
+  // Simple car state
+  enum CarState { stay_in_lane, changing_lane };
+  CarState car_state = CarState::stay_in_lane;
+
   string line;
   while (getline(in_map_, line)) {
   	istringstream iss(line);
@@ -196,8 +209,9 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy, &lane, &ref_vel, &car_state](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
+
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -231,15 +245,232 @@ int main() {
           	double end_path_d = j[1]["end_path_d"];
 
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
-          	auto sensor_fusion = j[1]["sensor_fusion"];
+			auto sensor_fusion = j[1]["sensor_fusion"];
 
-          	json msgJson;
+			int prev_size = previous_path_x.size();
 
-          	vector<double> next_x_vals;
-          	vector<double> next_y_vals;
+			if (prev_size > 0) {
+				car_s = end_path_s;
+			}
+			
+			// Variables for our lane change/follow logic below
+			bool too_close = false;
+			bool safe_to_change_left = true;
+			bool safe_to_change_right = true;
 
+			double left_change_space = 1000.;
+			double right_change_space = 1000.;
+			double follow_speed = 1000.;
+			double check_distance = 1000.;
+			
+			// Loop through our sensor fusion data about the cars around us
+			// Data is in the following format: [ id, x, y, vx, vy, s, d]
+			for (int i = 0; i < sensor_fusion.size(); i++) {
+				float d = sensor_fusion[i][6];
+				double vx = sensor_fusion[i][3];
+				double vy = sensor_fusion[i][4];
+				double check_speed = sqrt(vx * vx + vy * vy);
+				double check_car_s = sensor_fusion[i][5];
+				
+				check_car_s += ((double) prev_size * 0.02 * check_speed);
 
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+				// If the car is within our lane
+				if (d < (2 + 4 * lane + 2) && d > (2 + 4 * lane - 2)) {
+
+					// We want to keep track of the distance to the nearest car
+					check_distance = std::min(check_distance, check_car_s - car_s);
+
+					// If we are too close to this car
+					if ((check_car_s > car_s) && ((check_car_s - car_s) < 30)) {
+
+						too_close = true;
+						
+						// Save the speed of this car as we might want to follow it
+						follow_speed = std::min(follow_speed, check_speed);
+					}
+				} else {
+					// For cars in other lanes
+
+					// If I'm on the left most lane, it's not safe to change left (same for changing right on rightmost lane)
+					if (lane == 0) safe_to_change_left = false;
+					if (lane == 2) safe_to_change_right = false;
+
+					// If there's are available lanes to the left,
+					// AND the car we're checking is in that lane
+					// AND it's a safe distance away
+					if ((lane > 0) && 
+						(d < (2 + 4 * (lane - 1) + 2)) &&
+						(d > (2 + 4 * (lane - 1) - 2))) {
+
+						left_change_space = std::min(left_change_space, check_car_s - car_s);
+						
+						if (((check_car_s - car_s) < 30) && ((check_car_s - car_s) > -20)) {
+							safe_to_change_left = false;
+						}
+					}
+
+					// If there's an available lane to the right
+					// AND the car we're checking is in that lane
+					// AND it's a safe distance away
+					if ((lane < 2) &&
+						(d < (2 + 4 * (lane + 1) + 2)) &&
+						(d > (2 + 4 * (lane + 1) - 2))) {
+
+						right_change_space = std::min(right_change_space, check_car_s - car_s);
+
+						if (((check_car_s - car_s) < 30) && ((check_car_s - car_s) > -20)) {
+							safe_to_change_right = false;
+						}
+					}
+				}
+			}
+			
+			// Work out the current lane
+			int current_lane = car_d / 4;
+			if (current_lane == lane) {
+				car_state = CarState::stay_in_lane;
+			}
+
+			// If we are too close to the car in front
+			if (too_close) {
+				// Check if we can change lanes
+				if (car_state != CarState::changing_lane) {
+					// If both lanes are safe to change into,
+					// Pick the one that has the most space
+					if (safe_to_change_left && safe_to_change_right) {
+						if (left_change_space > right_change_space) {
+							safe_to_change_right = false;
+						} else {
+							safe_to_change_left = false;
+						}
+					}
+
+					// If it is safe to change lanes,
+					// set our car state to changing_lane
+					// and increment/decrement our target lane accordingly
+					if (safe_to_change_left) {
+						car_state = CarState::changing_lane;
+						lane--;
+					} else if (safe_to_change_right) {
+						car_state = CarState::changing_lane;
+						lane++;
+					} else {
+						// Else it is not safe to change lanes, so we slow down instead
+						// We slow down to the speed of the car in front of us, or the
+						// speed limit, whichever is lower
+						if (ref_vel > std::min(follow_speed, SPEED_LIMIT)) {
+							ref_vel -= 0.224;
+						}
+
+						// If we are too close, apply EMERGENCY BRAKES!
+						if (check_distance < 10) {
+							ref_vel = 0;
+						}
+					}
+				}
+			} else {
+				// If we are below the speed limit, accelerate
+				if (ref_vel < SPEED_LIMIT) {
+					ref_vel += 0.224;
+				}
+			}
+			
+
+			vector<double> ptsx;
+			vector<double> ptsy;
+
+			double ref_x = car_x;
+			double ref_y = car_y;
+			double ref_yaw = deg2rad(car_yaw);
+
+			// If the previous size is almost empty, use the car's position as a starting reference
+			if (prev_size < 2) {
+				double prev_car_x = car_x - cos(car_yaw);
+				double prev_car_y = car_y - sin(car_yaw);
+
+				ptsx.push_back(prev_car_x);
+				ptsx.push_back(car_x);
+
+				ptsy.push_back(prev_car_y);
+				ptsy.push_back(car_y);
+			} else {
+				// Else, use the previous path's end point as a starting reference
+				ref_x = previous_path_x[prev_size - 1];
+				ref_y = previous_path_y[prev_size - 1];
+
+				double ref_x_prev = previous_path_x[prev_size - 2];
+				double ref_y_prev = previous_path_y[prev_size - 2];
+				ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+				ptsx.push_back(ref_x_prev);
+				ptsx.push_back(ref_x);
+
+				ptsy.push_back(ref_y_prev);
+				ptsy.push_back(ref_y);
+			}
+
+			vector<double> next_wp0 = getXY(car_s + 30, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			vector<double> next_wp1 = getXY(car_s + 60, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			vector<double> next_wp2 = getXY(car_s + 90, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+			ptsx.push_back(next_wp0[0]);
+			ptsx.push_back(next_wp1[0]);
+			ptsx.push_back(next_wp2[0]);
+			
+			ptsy.push_back(next_wp0[1]);
+			ptsy.push_back(next_wp1[1]);
+			ptsy.push_back(next_wp2[1]);
+
+			for (int i = 0; i < ptsx.size(); i++) {
+				double shift_x = ptsx[i] - ref_x;
+				double shift_y = ptsy[i] - ref_y;
+
+				ptsx[i] = (shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw));
+				ptsy[i] = (shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw));
+			}
+
+			tk::spline s;
+
+			// Add the points for the spline to fit through
+			s.set_points(ptsx, ptsy);
+			
+			vector<double> next_x_vals;
+			vector<double> next_y_vals;
+
+			for (int i = 0; i < previous_path_x.size(); i++) {
+				next_x_vals.push_back(previous_path_x[i]);
+				next_y_vals.push_back(previous_path_y[i]);
+			}
+
+			double target_x = 30.0;
+			double target_y = s(target_x);
+			double target_dist = sqrt((target_x * target_x) + (target_y * target_y));
+
+			double x_add_on = 0;
+
+			for (int i = 1; i < 50 - previous_path_x.size(); i++) {
+
+				double N = (target_dist / (0.02 * ref_vel / 2.24)); // 2.24 converts ref_vel from miles/hour to meters/second
+				double x_point = x_add_on + (target_x) / N;
+				double y_point = s(x_point);
+
+				x_add_on = x_point;
+
+				double x_ref = x_point;
+				double y_ref = y_point;
+
+				x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw));
+				y_point = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw));
+
+				x_point += ref_x;
+				y_point += ref_y;
+
+				next_x_vals.push_back(x_point);
+				next_y_vals.push_back(y_point);
+
+			}
+			
+			json msgJson;
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
